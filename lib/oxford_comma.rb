16b407af2247235fc11cecac87332ff3f9307cfe
Koy Saeteurn <kosatun@aol.com>
2018-07-25 22:57:54 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,13 +2,11 @@ def oxford_comma(array)
   case array
     when array.size == 1 
       array.join
-    when    
-  
-  
-  
-  
-  
-  
-  
+    when array.size == 2 
+      array.join(" and ")
+    when array.size == 3
+      array.join(", and ")
+    when array.size > 3
+      array.join(", ")
   end
 end
