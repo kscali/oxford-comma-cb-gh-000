8a696aee865620215cbc0a0dcd8dc74d932101aa
Koy Saeteurn <kosatun@aol.com>
2018-07-25 23:11:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,14 @@
 def oxford_comma(array)
-  case array
-    when array.length == 1 
+  if array.length == 1 
       array.join("")
-    when array.length == 2 
+  elsif array.length == 2 
       two = array.join(" and ")
-       puts two
-    when array.length == 3
+       two
+  elsif array.length == 3
       three =  array.join(", and ")
-      puts three
-    when array.length > 3
+      three
+  elsif array.length > 3
       more = array.join(", ")
-      puts more
+       more
   end
 end
