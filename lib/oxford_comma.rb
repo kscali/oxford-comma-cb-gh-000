fc487f24eec5d6acc04c5ca0c4a587bdfdf99cd6
Koy Saeteurn <kosatun@aol.com>
2018-07-25 22:59:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
 def oxford_comma(array)
   case array
     when array.size == 1 
-      array.join
+      puts array.join
     when array.size == 2 
-      array.join(" and ")
+      puts array.join(" and ")
     when array.size == 3
-      array.join(", and ")
+      puts array.join(", and ")
     when array.size > 3
-      array.join(", ")
+      puts array.join(", ")
   end
 end
