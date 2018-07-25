02072dbdea00c51c6eb55cdf7176ba33fc3b457c
Koy Saeteurn <kosatun@aol.com>
2018-07-25 23:31:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,10 +5,10 @@ def oxford_comma(array)
       two = array.join(" and ")
        two
   elsif array.length == 3
-      three =  array[0..-1].join(",") + "and" + array[-1]
+      three =  array[0..-2].join(",") + "and" + array[-1]
       three
   elsif array.length > 3
-      more = array[0..-1].join(", ") + "and" + array[-1]
+      more = array[0..-2].join(", ") + "and" + array[-1]
       more
   end
 end
